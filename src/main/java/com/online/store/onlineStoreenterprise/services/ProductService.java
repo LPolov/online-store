d25dd86ad609682865ff788c1261d60c4b6f21c7
Prem Pun <premchhantyal2010@gmail.com>
2022-04-10 14:04:16 -0400

    product edit delete and error message added

--- src/main/java/com/online/store/onlineStoreenterprise/services/ProductService.java
+++ src/main/java/com/online/store/onlineStoreenterprise/services/ProductService.java
@@ -2,6 +2,7 @@ package com.online.store.onlineStoreenterprise.services;
 
 import com.online.store.onlineStoreenterprise.dao.ProductRepository;
 import com.online.store.onlineStoreenterprise.dto.SaveProductRequest;
+import com.online.store.onlineStoreenterprise.dto.UpdateProductRequest;
 import com.online.store.onlineStoreenterprise.models.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Service;
 import javax.persistence.EntityNotFoundException;
 import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -29,5 +31,26 @@ public class ProductService {
     public Product addProduct(SaveProductRequest product) {
         Product _product = productRepository.save(new Product(product));
         return _product;
+    }
+
+    @Transactional
+    public Product updateProduct(UUID id, UpdateProductRequest product) {
+        Optional<Product> productData = productRepository.findById(id);
+        if (productData.isPresent()) {
+            Product _product = productData.get();
+            _product.setName(product.getName());
+            _product.setPrice(product.getPrice());
+            _product.setBrand(product.getBrand());
+            _product.setCategories(product.getCategories());
+            _product.setStockQty(product.getStockQty());
+            _product.setDescription(product.getDescription());
+            return productRepository.save(_product);
+        } else {
+            return null;
+        }
+    }
+
+    public void deleteProduct(UUID id) {
+        productRepository.deleteById(id);
     }
 }
